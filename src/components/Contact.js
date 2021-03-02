a60210362a73ec6835c05691197221dfded77efe
Sarah <70615195+smanter82@users.noreply.github.com>
2021-03-02 16:33:10 -0500

    Started adding react-bootstrap to pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,55 +5,62 @@ import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Resume from "../Sarah_Manter_Resume.pdf";
+import backgroundImage from "../Images/tree_picture.png";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
 export default function Contact() {
   return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Body>
-          <Card.Title>
-            <h1>Contact Me:</h1>
-          </Card.Title>
-          <Card.Text>
-            <h2>Please contact me at any time. I'd love to chat!</h2>
-            <p>Email: sarahmanter@gmail.com</p>
-            <p>Phone: 603-496-4846</p>
-          </Card.Text>
-          <Button variant="success">
-            <Card.Link
-              target="blank"
-              href="https://www.linkedin.com/in/sarah-manter-40881877/"
-            >
-              Here is my LinkedIn Profile.
-            </Card.Link>
-          </Button>
-          <Button variant="success">
-            <Card.Link target="blank" href={Resume}>
-              My Resume
-            </Card.Link>
-          </Button>
-          <Button variant="success">
-            <Card.Link target="blank" href="https://github.com/smanter82/">
-              Here is my GitHub Repository.
-            </Card.Link>
-          </Button>
-          <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="name" placeholder="Your Name Here" />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
-          <Button variant="primary">Submit</Button>
-        </Card.Body>
-      </Card>
-    </>
+    <div style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <Container>
+        <Row className="align-items-flex-end">
+          <Card style={{ width: "75%" }}>
+            <Card.Body>
+              <Card.Title>
+                <h1>Contact Me:</h1>
+              </Card.Title>
+              <Card.Text>
+                <h2>Please contact me at any time. I'd love to chat!</h2>
+                <p className="padText">Email: sarahmanter@gmail.com</p>
+                <p className="padText">Phone: 603-496-4846</p>
+              </Card.Text>
+              <Button variant="success">
+                <Card.Link
+                  target="blank"
+                  href="https://www.linkedin.com/in/sarah-manter-40881877/"
+                >
+                  Here is my LinkedIn Profile.
+                </Card.Link>
+              </Button>
+              <Button variant="success">
+                <Card.Link target="blank" href={Resume}>
+                  My Resume
+                </Card.Link>
+              </Button>
+              <Button variant="success">
+                <Card.Link target="blank" href="https://github.com/smanter82/">
+                  Here is my GitHub Repository.
+                </Card.Link>
+              </Button>
+              <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="name" placeholder="Your Name Here" />
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+              </Form>
+              <Button variant="primary">Submit</Button>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
+    </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,39 +5,47 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import profilePic from "../Images/profile_pic.jpg";
 import familyPic from "../Images/kids_on_bridge.jpg";
+import backgroundImage from "../Images/tree_picture.png";
+import { CardColumns } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
 
 export default function HomePage() {
   return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={profilePic} />
-        <Card.Body>
-          <Card.Title>About Me:</Card.Title>
-          <Card.Text>
-            Hello! I graduated from the University of New Hampshire with a
-            Masters of Arts in Teaching Secondary English. I spent 13 years
-            teaching middle school Language Arts, but now it is time for a
-            change. I have always enjoyed working with computers. I am taking a
-            coding course through UNH, and now I look forward to using that
-            passion in the world of Web Design.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={familyPic} />
-        <Card.Body>
-          <Card.Title>My family</Card.Title>
-          <Card.Text>
-            I am the mother of amazing twins. While they take up a lot of time
-            and energy, they have been super supportive of my quest to dive into
-            Web Development. They stay out of my hair as much as they can while
-            I'm doing homework, and my daughter even practices learning to code
-            on her own computer next to me while I study.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </>
+    <div style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <Row>
+        <CardColumns>
+          <Card style={{ width: "30rem" }}>
+            <Card.Img variant="top" src={profilePic} />
+            <Card.Body>
+              <Card.Title>About Me:</Card.Title>
+              <Card.Text>
+                Hello! I graduated from the University of New Hampshire with a
+                Masters of Arts in Teaching Secondary English. I spent 13 years
+                teaching middle school Language Arts, but now it is time for a
+                change. I have always enjoyed working with computers. I am
+                taking a coding course through UNH, and now I look forward to
+                using that passion in the world of Web Design.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "30rem" }}>
+            <Card.Img variant="top" src={familyPic} />
+            <Card.Body>
+              <Card.Title>My family</Card.Title>
+              <Card.Text>
+                I am the mother of amazing twins. While they take up a lot of
+                time and energy, they have been super supportive of my quest to
+                dive into Web Development. They stay out of my hair as much as
+                they can while I'm doing homework, and my daughter even
+                practices learning to code on her own computer next to me while
+                I study.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </CardColumns>
+      </Row>
+    </div>
   );
 }
