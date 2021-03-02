6aec01885b67db2aacd779035992f546194e3bde
Sarah <70615195+smanter82@users.noreply.github.com>
2021-03-01 22:34:24 -0500

    Rebuilt original cards from responsive portfolio without formatting

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Contact from "./components/Contact";
+import HomePage from "./components/HomePage";
+import Portfolio from "./components/Portfolio";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <HomePage></HomePage>
+      <Portfolio></Portfolio>
+      <Contact></Contact>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "../styles/Contact.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Resume from "../Sarah_Manter_Resume.pdf";
+
+export default function Contact() {
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>
+            <h1>Contact Me:</h1>
+          </Card.Title>
+          <Card.Text>
+            <h2>Please contact me at any time. I'd love to chat!</h2>
+            <p>Email: sarahmanter@gmail.com</p>
+            <p>Phone: 603-496-4846</p>
+          </Card.Text>
+          <Button variant="success">
+            <Card.Link
+              target="blank"
+              href="https://www.linkedin.com/in/sarah-manter-40881877/"
+            >
+              Here is my LinkedIn Profile.
+            </Card.Link>
+          </Button>
+          <Button variant="success">
+            <Card.Link target="blank" href={Resume}>
+              My Resume
+            </Card.Link>
+          </Button>
+          <Button variant="success">
+            <Card.Link target="blank" href="https://github.com/smanter82/">
+              Here is my GitHub Repository.
+            </Card.Link>
+          </Button>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="name" placeholder="Your Name Here" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+          <Button variant="primary">Submit</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
