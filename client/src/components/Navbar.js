67506753a3369e1cfa3a3608096d5ad3646c0fed
Sarah <70615195+smanter82@users.noreply.github.com>
2021-03-26 16:38:57 -0400

    added containers and responsive navbar to homepage

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,21 +5,24 @@ import "../styles/Navbar.css";
 export default function PortfolioNavbar() {
   return (
     <>
-      <Navbar>
+      <Navbar collapseOnSelect expand="lg">
         <Navbar.Brand className="nameStyle px-4" href="/">
           Sarah Manter
         </Navbar.Brand>
-        <Nav className="ml-auto px-4">
-          <Nav.Link className="greenLinks" href="/">
-            Home Page
-          </Nav.Link>
-          <Nav.Link className="greenLinks" href="Portfolio">
-            Portfolio
-          </Nav.Link>
-          <Nav.Link className="greenLinks" href="Contact">
-            Contact
-          </Nav.Link>
-        </Nav>
+        <Navbar.Toggle id="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto px-4">
+            <Nav.Link className="greenLinks" href="/">
+              Home Page
+            </Nav.Link>
+            <Nav.Link className="greenLinks" href="Portfolio">
+              Portfolio
+            </Nav.Link>
+            <Nav.Link className="greenLinks" href="Contact">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
